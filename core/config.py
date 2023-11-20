0bdfec16425b80fbc85ea6f0a336515427b2fa88
c0nfid <beiov2004.mail@gmail.com>
2023-11-21 00:31:07 +0300

    Add load db address from env

--- core/config.py
+++ core/config.py
@@ -1,8 +1,12 @@
+import os
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
-    db_url: str = f"postgresql+asyncpg://postgres:root@"
+    db_url: str = f"{os.environ.get('DB_ADDRESS')}"
     db_echo: bool = True
 
 
